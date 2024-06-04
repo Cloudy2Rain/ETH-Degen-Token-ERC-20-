16df308023ced3af0fe5b55c678925d01b0d30ac
Cloudy2Rain <128135990+Cloudy2Rain@users.noreply.github.com>
2024-06-05 04:24:40 +0800

    Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -2,19 +2,22 @@
 pragma solidity ^0.8.10;
 
 contract Ownable {
-    address public ian;
+    address public owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor() {
-        ian = msg.sender;
+        owner = msg.sender;
     }
-    modifier onlyIan() {
-        require(msg.sender == ian, "Ownable: caller is not the owner");
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
         _;
     }
-    function transferOwnership(address newOwner) public onlyIan {
-        emit OwnershipTransferred(ian, newOwner);
-        ian = newOwner;
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
     }
 }
